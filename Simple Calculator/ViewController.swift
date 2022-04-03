8f088a62781a825214e777a6288c90017335fec2
hendra <troblang212@gmail.com>
2022-04-03 18:34:49 +0800

    Initial Commit

---
+++ Simple Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by hendra on 03/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
